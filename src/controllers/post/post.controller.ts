9a0c6715760ecb20de66494887722eca2050de30
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-02-09 20:27:02 +0700

    123123

--- src/controllers/post/post.controller.ts
+++ src/controllers/post/post.controller.ts
@@ -1,6 +1,8 @@
 import {
     Body,
     Controller,
+    Delete,
+    Param,
     Patch,
     Post,
     Put,
@@ -13,7 +15,17 @@ import PostService from "./post.service"
 import { UserMySqlEntity } from "@database"
 import { FileFieldsInterceptor } from "@nestjs/platform-express"
 import { JwtAuthGuard, AuthInterceptor, UserId, DataFromBody } from "../shared"
-import { CreateCommentData, CreatePostData, ReactPostData, UpdateCommentData, UpdatePostData, createCommentSchema, createPostSchema, updateCommentSchema, updatePostSchema } from "./shared"
+import {
+    CreateCommentData,
+    CreatePostData,
+    ReactPostData,
+    UpdateCommentData,
+    UpdatePostData,
+    createCommentSchema,
+    createPostSchema,
+    updateCommentSchema,
+    updatePostSchema,
+} from "./shared"
 import { Files } from "@common"
 
 @ApiTags("Post")
@@ -38,70 +50,89 @@ export default class PostController {
         return await this.postService.createPost({ userId, data, files })
     }
 
-    @ApiBearerAuth()
-    @ApiConsumes("multipart/form-data")
-    @ApiBody({ schema: updatePostSchema })
-    @Put("update-post")
-    @UseGuards(JwtAuthGuard)
-    @UseInterceptors(
-      AuthInterceptor<UserMySqlEntity>,
-      FileFieldsInterceptor([{ name: "files" }]),
-    )
+  @ApiBearerAuth()
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({ schema: updatePostSchema })
+  @Put("update-post")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    AuthInterceptor<UserMySqlEntity>,
+    FileFieldsInterceptor([{ name: "files" }]),
+  )
   async updatePost(
-      @UserId() userId: string,
-      @DataFromBody() data: UpdatePostData,
-      @UploadedFiles() { files }: Files,
+    @UserId() userId: string,
+    @DataFromBody() data: UpdatePostData,
+    @UploadedFiles() { files }: Files,
   ) {
       return await this.postService.updatePost({ userId, data, files })
   }
 
   @ApiBearerAuth()
+  @Delete("delete-post")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(AuthInterceptor<UserMySqlEntity>)
+  async deletePost(@UserId() userId: string, @Param("postId") postId: string) {
+      console.log(postId)
+  }
+
+  @ApiBearerAuth()
   @Patch("react-post")
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(
-      AuthInterceptor<UserMySqlEntity>,
-  )
-    async reactPost(
-      @UserId() userId: string,
-      @Body() body: ReactPostData
-    ) {
-        return this.postService.reactPost({
-            userId,
-            data: body
-        })
-    }
+  @UseInterceptors(AuthInterceptor<UserMySqlEntity>)
+  async reactPost(@UserId() userId: string, @Body() body: ReactPostData) {
+      return this.postService.reactPost({
+          userId,
+          data: body,
+      })
+  }
 
-    @ApiBearerAuth()
-    @ApiConsumes("multipart/form-data")
-    @ApiBody({ schema: createCommentSchema })
-    @Post("create-comment")
-    @UseGuards(JwtAuthGuard)
-    @UseInterceptors(
-      AuthInterceptor<UserMySqlEntity>,
-      FileFieldsInterceptor([{ name: "files" }]),
-    )
+  @ApiBearerAuth()
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({ schema: createCommentSchema })
+  @Post("create-comment")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    AuthInterceptor<UserMySqlEntity>,
+    FileFieldsInterceptor([{ name: "files" }]),
+  )
   async createComment(
-      @UserId() userId: string,
-      @DataFromBody() data: CreateCommentData,
-      @UploadedFiles() { files }: Files,
+    @UserId() userId: string,
+    @DataFromBody() data: CreateCommentData,
+    @UploadedFiles() { files }: Files,
   ) {
       return await this.postService.createComment({ userId, data, files })
   }
 
   @ApiBearerAuth()
-    @ApiConsumes("multipart/form-data")
-    @ApiBody({ schema: updateCommentSchema })
-    @Put("update-comment")
-    @UseGuards(JwtAuthGuard)
-    @UseInterceptors(
-      AuthInterceptor<UserMySqlEntity>,
-      FileFieldsInterceptor([{ name: "files" }]),
-    )
-    async updateComment(
-      @UserId() userId: string,
-      @DataFromBody() data: UpdateCommentData,
-      @UploadedFiles() { files }: Files,
-    ) {
-        return await this.postService.updateComment({ userId, data, files })
-    }
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({ schema: updateCommentSchema })
+  @Put("update-comment")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    AuthInterceptor<UserMySqlEntity>,
+    FileFieldsInterceptor([{ name: "files" }]),
+  )
+  async updateComment(
+    @UserId() userId: string,
+    @DataFromBody() data: UpdateCommentData,
+    @UploadedFiles() { files }: Files,
+  ) {
+      return await this.postService.updateComment({ userId, data, files })
+  }
+
+  @ApiBearerAuth()
+  @Delete("delete-comment")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(AuthInterceptor<UserMySqlEntity>)
+  async deleteComment(@UserId() userId: string, @Param("commentId") postId: string) {
+      console.log(postId)
+  }
+
+  @ApiBearerAuth()
+  @Patch("react-comment")
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(AuthInterceptor<UserMySqlEntity>)
+  async reactComment(@UserId() userId: string, @Body() body: ReactPostData) {
+      return null
+  }
 }
