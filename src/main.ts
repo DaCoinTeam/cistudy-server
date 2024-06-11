dd551f964fd48d3117146c3be6d70e2359ffae37
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-06-11 11:50:30 +0700

    1232

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
-import { appConfig, keysConfig }  from "@config"
+import { appConfig, keysConfig } from "@config"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import {
-    CoursesResolver,
-    PostsResolver,
-    ProfileResolver,
-    UsersResolver,
-    AuthResolver,
-    TransactionsResolver,
-    CartResolver,
+  CoursesResolver,
+  PostsResolver,
+  ProfileResolver,
+  UsersResolver,
+  AuthResolver,
+  TransactionsResolver,
+  CartResolver,
 } from "@resolvers"
 import { promises as fsPromises, readFileSync } from "fs"
 import {
-    GraphQLSchemaBuilderModule,
-    GraphQLSchemaFactory,
+  GraphQLSchemaBuilderModule,
+  GraphQLSchemaFactory,
 } from "@nestjs/graphql"
 import { printSchema } from "graphql"
 import { join } from "path"
@@ -23,64 +23,61 @@ import { RedisIoAdapter } from "@adapters"
 import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface"
 
 const generateSchema = async () => {
-    const app = await NestFactory.create(GraphQLSchemaBuilderModule)
-    await app.init()
+  const app = await NestFactory.create(GraphQLSchemaBuilderModule)
+  await app.init()
 
-    const gqlSchemaFactory = app.get(GraphQLSchemaFactory)
-    const schema = await gqlSchemaFactory.create([
-        AuthResolver,
-        CoursesResolver,
-        PostsResolver,
-        UsersResolver,
-        ProfileResolver,
-        TransactionsResolver,
-        CartResolver
-    ])
-    await fsPromises.writeFile(
-        join(
-            process.cwd(),
-            `${getEnvValue({ development: "src", production: "dist" })}/schema.gql`,
-        ),
-        printSchema(schema),
-    )
+  const gqlSchemaFactory = app.get(GraphQLSchemaFactory)
+  const schema = await gqlSchemaFactory.create([
+    AuthResolver,
+    CoursesResolver,
+    PostsResolver,
+    UsersResolver,
+    ProfileResolver,
+    TransactionsResolver,
+    CartResolver,
+  ])
+  await fsPromises.writeFile(
+    join(
+      process.cwd(),
+      `${getEnvValue({ development: "src", production: "dist" })}/schema.gql`,
+    ),
+    printSchema(schema),
+  )
 }
 
 const bootstrap = async () => {
-    const httpsOptions: HttpsOptions = getEnvValue({
-        production: {
-            cert: keysConfig().ssl.cert,
-            key: keysConfig().ssl.key,
-        },
-    })
+  const httpsOptions: HttpsOptions = getEnvValue({
+    production: {
+      cert: keysConfig().ssl.cert,
+      key: keysConfig().ssl.key,
+    },
+  })
 
-    const app = await NestFactory.create(
-        AppModule,
-    //, {
-    //httpsOptions
-    //}
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  })
+
+  app.enableCors()
+
+  const redisIoAdapter = new RedisIoAdapter(app)
+  await redisIoAdapter.connectToRedis()
+  app.useWebSocketAdapter(redisIoAdapter)
+
+  const config = new DocumentBuilder()
+    .setTitle("CiStudy Server")
+    .setDescription(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhN2JiNTU4Zi04YTBjLTQzMGEtYTc0MS05ODZhNTBhZTE0NzMiLCJ0eXBlIjoiUmVmcmVzaCIsImlhdCI6MTcwNzQ2MjUxNSwiZXhwIjoxNzEwMDU0NTE1fQ.SsZFDKCHjn7i06L_j5WSkTEdGUu0tQ0txYyyl3l5oCc",
     )
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
 
-    app.enableCors()
+  SwaggerModule.setup("/", app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  })
 
-    const redisIoAdapter = new RedisIoAdapter(app)
-    await redisIoAdapter.connectToRedis()
-    app.useWebSocketAdapter(redisIoAdapter)
-
-    const config = new DocumentBuilder()
-        .setTitle("CiStudy Server")
-        .setDescription(
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhN2JiNTU4Zi04YTBjLTQzMGEtYTc0MS05ODZhNTBhZTE0NzMiLCJ0eXBlIjoiUmVmcmVzaCIsImlhdCI6MTcwNzQ2MjUxNSwiZXhwIjoxNzEwMDU0NTE1fQ.SsZFDKCHjn7i06L_j5WSkTEdGUu0tQ0txYyyl3l5oCc",
-        )
-        .setVersion("1.0")
-        .addBearerAuth()
-        .build()
-    const document = SwaggerModule.createDocument(app, config)
-
-    SwaggerModule.setup("/", app, document, {
-        swaggerOptions: { defaultModelsExpandDepth: -1 },
-    })
-
-    await app.listen(appConfig().port || 3001)
+  await app.listen(appConfig().port || 3001)
 }
 
 generateSchema().then(() => bootstrap())
