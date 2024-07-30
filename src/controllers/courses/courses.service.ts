a888f68f0fbb0c482413efd008ad0cd2c6dd66e8
starci183 <cuongnvtse160875@gmail.com>
2024-07-30 22:27:06 +0700

    123123

--- src/controllers/courses/courses.service.ts
+++ src/controllers/courses/courses.service.ts
@@ -1147,6 +1147,7 @@ export class CoursesService {
                 courseCategories,
             )
 
+            await queryRunner.commitTransaction()
             return {
                 message: "Course Category has been created successfully",
             }
