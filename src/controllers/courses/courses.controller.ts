127f7a23812b14da465e4cee3ebe73dd4bfe6aea
neoxss3 <ss3xneo@gmail.com>
2024-08-21 23:27:16 +0700

    123

--- src/controllers/courses/courses.controller.ts
+++ src/controllers/courses/courses.controller.ts
@@ -84,7 +84,7 @@ export class CoursesController {
     @ApiBearerAuth()
     @Post("create-course")
     @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(SystemRoles.User, SystemRoles.Instructor)
+    @Roles(SystemRoles.User)
     @UseInterceptors(AuthInterceptor)
     async createCourse(@AccountId() accountId: string) {
         return await this.coursesService.createCourse({
